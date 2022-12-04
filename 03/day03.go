6a0952b44c06456df75ca0fa4cdd9629b13bfc10
Loraine Kanervisto <loraine@lorainekv.com>
2022-12-04 10:13:54 -0800

    Don't anticipate too many future changes.

--- 03/day03.go
+++ 03/day03.go
@@ -6,64 +6,7 @@ import (
   "github.com/ghetzel/go-stockutil/sliceutil"
 )
 
-var lowerCasePri = map[string] int {
-  "a": 1,
-  "b": 2,
-  "c": 3,
-  "d": 4,
-  "e": 5,
-  "f": 6,
-  "g": 7,
-  "h": 8,
-  "i": 9,
-  "j": 10,
-  "k": 11,
-  "l": 12,
-  "m": 13,
-  "n": 14,
-  "o": 15,
-  "p": 16,
-  "q": 17,
-  "r": 18,
-  "s": 19,
-  "t": 20,
-  "u": 21,
-  "v": 22,
-  "w": 23,
-  "x": 24,
-  "y": 25,
-  "z": 26,
-}
-
-var upperCasePri = map[string] int {
-  "A": 27,
-  "B": 28,
-  "C": 29,
-  "D": 30,
-  "E": 31,
-  "F": 32,
-  "G": 33,
-  "H": 34,
-  "I": 35,
-  "J": 36,
-  "K": 37,
-  "L": 38,
-  "M": 39,
-  "N": 40,
-  "O": 41,
-  "P": 42,
-  "Q": 43,
-  "R": 44,
-  "S": 45,
-  "T": 46,
-  "U": 47,
-  "V": 48,
-  "W": 49,
-  "X": 50,
-  "Y": 51,
-  "Z": 52,
-}
-
+const priorityOrder = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func PartOne(inputList string) int {
   prioritySum := 0
@@ -105,13 +48,8 @@ func PartTwo(inputList string) int {
 }
 
 func getPriority(letter string) int {
-  isUpper := letter == strings.ToUpper(letter)
-
-  if isUpper {
-    return upperCasePri[letter]
-  } else {
-    return lowerCasePri[letter]
-  }
+  idx := strings.Index(priorityOrder, letter)
+  return idx + 1
 }
 
 func dupeFinder(compartments_pair [2]string) []string {
