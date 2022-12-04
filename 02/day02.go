2018533621d275d697e02ac77d0de10755769702
Loraine Kanervisto <loraine@lorainekv.com>
2022-12-03 17:08:49 -0800

    Green scoreRound tests

--- 02/day02.go
+++ 02/day02.go
@@ -1,12 +1,52 @@
 package main
 import (
+  "strings"
 )
+
+var opponentMineMapping = map[string]string {
+  "A": "X",
+  "B": "Y",
+  "C": "Z",
+}
+
+var moveToWinningMove = map[string]string {
+  "A": "Y",
+  "B": "Z",
+  "C": "X",
+}
+
+var playerMoveToPoints = map[string]int {
+  "X": 1,
+  "Y": 2,
+  "Z": 3,
+}
 
 func PartOne(inputList string) int {
   return 0
 }
 
 func scoreRound(roundString string) int {
-  return 0
+  roundSlice := strings.Split(roundString, " ")
+  opponentMove := roundSlice[0]
+  myMove := roundSlice[1]
+
+  myMovePoints := playerMoveToPoints[myMove]
+
+  isDraw := opponentMineMapping[opponentMove] == myMove
+
+  winningResponse := moveToWinningMove[opponentMove]
+  isWin := myMove == winningResponse
+
+
+  roundOutcome := 0
+  if isDraw { // draw
+    roundOutcome = 3
+  } else if isWin {
+    roundOutcome = 6
+  } else {
+    roundOutcome = 0
+  }
+
+  return roundOutcome + myMovePoints
 }
 
