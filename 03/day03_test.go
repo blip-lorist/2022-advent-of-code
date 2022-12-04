d19f67984e954c35b9d893086fdc8aab3f18b3eb
Loraine Kanervisto <loraine@lorainekv.com>
2022-12-03 18:40:18 -0800

    started priority mappings

--- 03/day03_test.go
+++ 03/day03_test.go
@@ -17,3 +17,18 @@ CrZsJsPPZsGzwwsLwLmpwMDw`
     t.Errorf("got %d, wanted %d", actual, expected)
   }
 }
+
+func TestGetPriority(t *testing.T){
+  testInputs := [6]string{"p", "L", "P", "v", "t", "s"}
+
+  expectedOutputs := [6]int{16, 38, 42, 22, 20, 19}
+
+  for i := 0; i < len(testInputs); i++ {
+    expected := expectedOutputs[i]
+    actual := getPriority(testInputs[i])
+
+    if expected != actual {
+      t.Errorf("got %d, wanted %d", actual, expected)
+    }
+  }
+}
